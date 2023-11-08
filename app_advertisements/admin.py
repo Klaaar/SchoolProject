8fa49c1a2501dd1272b41ebcbf2c8164db38a701
Klar <klar.osadkinaWgmail.com>
2023-11-08 20:19:42 +0300

    project

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Advertisement
-
+#логин admin
+#пароль password
 # Register your models here.
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = [ 'id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date', 'get_html_image', 'user' ]
